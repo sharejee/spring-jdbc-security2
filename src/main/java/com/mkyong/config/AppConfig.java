b6af607d798f42696d1f91f08c1485154bf3eb96
r.ron@gl-f.com <r.ron@gl-f.com>
2018-02-12 13:44:01 +0700

    refector folder

--- src/main/java/com/mkyong/config/AppConfig.java
+++ src/main/java/com/mkyong/config/AppConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan({ "com.mkyong.web.*" })
+@ComponentScan({ "com.mkyong.domain.*" })
 @Import({ SecurityConfig.class })
 public class AppConfig {
 
--- src/main/java/com/mkyong/domain/controllers/MainController.java
+++ src/main/java/com/mkyong/domain/controllers/MainController.java
@@ -1,4 +1,4 @@
-package com.mkyong.web.controller;
+package com.mkyong.domain.controllers;
 
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
