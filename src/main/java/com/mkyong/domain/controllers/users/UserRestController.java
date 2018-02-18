a96e57282a9db0199540ce5b187011b4f8378b54
Rith <rithronlkh@gmail.com>
2018-02-18 13:49:23 +0700

    add swagger

--- src/main/java/com/mkyong/domain/controllers/users/UserRestController.java
+++ src/main/java/com/mkyong/domain/controllers/users/UserRestController.java
@@ -13,12 +13,10 @@ Create Date: 2/12/2018
 */
 @RestController
 public class UserRestController {
-   /* AbstractApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-    SecUserService secUserService = (SecUserService) context.getBean("secUserService");*/
    @Autowired
    public SecUserService secUserService;
 
-    @GetMapping(value = "/api/users")
+    @RequestMapping(value = "/api/users",method = RequestMethod.GET)
     public List<SecUser> getAllUsers() {
         List<SecUser> secUsers =  (List<SecUser>) secUserService.getAllUsers();
         return secUsers;
